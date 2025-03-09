1cbbb92951d56439dcb2c0d2ceb7d58f34a6ac7f
HansHugoHMB <mbayahans@gmail.com>
2025-03-09 11:58:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-document.getElementById('printButton').addEventListener('click', function() {
+document.getElementById('printBtn').addEventListener('click', function() {
+    // Solution universelle
+    if (navigator.userAgent.toLowerCase().includes('android')) {
+        // Pour WebView Android
+        window.print = function() {
+            alert("Utilisez le menu partage → Imprimer");
+        };
+    }
     window.print();
 });
