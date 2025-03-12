15cfe6869c12e8d00eff1a781addc1de127fe851
HansHugoHMB <mbayahans@gmail.com>
2025-03-12 17:44:29 +0100

    Update token.js

--- config/token.js
+++ config/token.js
@@ -1,6 +1,6 @@
 // config/token.js
 const config = {
-  GITHUB_TOKEN: 'github_pat_11BPDCGEY05HGRAnqieARL_7oscbpiQod2JXIQLNLijqWBsKA5G9o3w3FwcnxNUbftRXE7U6HEMu4xMGJ'
+  GITHUB_TOKEN: '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'
 };
 
 export default config;
